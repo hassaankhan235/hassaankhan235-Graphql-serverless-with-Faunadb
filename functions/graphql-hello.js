4ec3c153446aed6e917f3b5ff0d19aae9043a2b2
hassaankhan235 <hassaankhan235@gmail.com>
2020-12-20 01:07:33 +0300

    Write to faunadb through graphql serverless

--- functions/graphql-hello.js
+++ functions/graphql-hello.js
@@ -1,24 +1,48 @@
-
 const {ApolloServer, gql} = require('apollo-server-lambda')
+const faunadb = require('faunadb'),
+q = faunadb.query;
 
 const typeDefs = gql`
-type strange {
-    name : String
+type user {
+    name: String,
     age: Int
 }
+
 type Query {
-    message: String,
-    getUsr: strange
+    User : user,
+    msg : String
+}
+
+type Mutation {
+    adddata(title: String) : String
 }
 `;
 
+
 const resolvers = {
-Query: {
-    message: () => "Hello from hassaan",
-    getUsr: () => ({name:'Hassaan', age:30}),
+    Query : {
+        msg : async() => {
+            console.log('Connecting to fauna')
+            try{
+                console.log('Connecting to fauna')
+                var client = new faunadb.Client({secret: 'fnAD8k-3RiACB8K1iVEw5OgNm-JRLgWZHOuYAd7v'})
+                let res = await client.query(
+                    q.Get(q.Ref(q.Collection('posts'), '284561364268090887'))
+                )
+                return res.data.title
+            }
+            catch(err){
+                err.toString()
+            }
+        },
+    },
+    Mutation:{
+        adddata: (_,title) => {
+            console.log('TITLE RECIEVED',title)
+            return('DOne')
+        }
     }
 }
-;
 
 const server = new ApolloServer({
     typeDefs,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,41 @@
 import React from "react"
-import { useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
 
 const APOLLO_Query = gql`
-{
-  message
-  getUsr{
-    name
-    age
+  {
+    msg
   }
-}
 `;
 
+const addData = gql`
+mutation AddData($title: String!){
+  adddata(title: $title)
+}
+`
+
 export default function Home() {
+  
+  const [adddata] = useMutation(addData)
   const { loading, error, data } = useQuery(APOLLO_Query)
   
   return <div>
   <h1> Data From ApolloClient </h1>
   {loading && <p> Loading message</p>}
   {error && <p> Error Loading </p>}
-  {data && 
-    <div> 
-    {data.message} 
-    <div> {data.getUsr.name} {data.getUsr.age} </div>
-    </div>}
+  {data && <div> {data.msg} </div>}
+  {console.log('returned', data)}
+  <div>
+  <form onSubmit = {
+    async (e) => {
+      e.preventDefault()
+      const ret = await adddata({variables: {title: 'Mastering Graphic Fauna'}})
+      console.log("BUTTON PUSHED")
+    } 
+  } >
+  <button type='submit'> Add </button>
+  </form>
   </div>
+  </div>
+
 }
